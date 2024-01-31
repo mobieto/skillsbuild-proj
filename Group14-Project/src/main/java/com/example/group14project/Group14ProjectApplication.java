f29d6cbf8237a3fb58da2be7bc20f6111c5980f4
David Orimoyegun <dorimoyegun@outlook.com>
2024-01-31 12:06:49 +0000

    Created project directory

---
+++ Group14-Project/src/main/java/com/example/group14project/Group14ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.group14project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group14ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group14ProjectApplication.class, args);
+    }
+
+}
