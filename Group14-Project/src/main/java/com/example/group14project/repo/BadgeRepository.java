dee38d45624c668933897b88230cefb878dd0c2b
M M Hosseini <mh675@student.le.ac.uk>
2024-02-15 16:07:42 +0000

    Update BadgeRepository.java

--- Group14-Project/src/main/java/com/example/group14project/repo/BadgeRepository.java
+++ Group14-Project/src/main/java/com/example/group14project/repo/BadgeRepository.java
@@ -2,9 +2,10 @@ package com.example.group14project.repo;
 
 import com.example.group14project.domain.Badge;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface BadgeRepository extends CrudRepository<Badge, Integer> {
     Badge findByName(String name);
 
-    Badge findByOwnerName(String name);
+    List<Badge> findAllByOwnerName(String name);
 }
