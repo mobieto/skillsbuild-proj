c0c98d2e550fa9a897b5b7783ebe43e973e12246
David Orimoyegun <dorimoyegun@outlook.com>
2024-02-15 17:52:39 +0000

    bug fixes

--- Group14-Project/src/main/java/com/example/group14project/controller/DashboardController.java
+++ Group14-Project/src/main/java/com/example/group14project/controller/DashboardController.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+
 @Controller
-public class MainController {
+public class DashboardController {
     @Autowired
     private CourseRepository courseRepository;
 
