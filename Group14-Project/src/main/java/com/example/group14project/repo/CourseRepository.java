0ffe3f09e39e65b84202c1083ea9011b7c91f86b
mp704 <mp704@student.le.ac.uk>
2024-03-13 13:57:40 +0000

    no change made

--- Group14-Project/src/main/java/com/example/group14project/repo/CourseRepository.java
+++ Group14-Project/src/main/java/com/example/group14project/repo/CourseRepository.java
@@ -4,6 +4,8 @@ package com.example.group14project.repo;
 import com.example.group14project.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Integer> {
     Course findByName(String name);
 }
